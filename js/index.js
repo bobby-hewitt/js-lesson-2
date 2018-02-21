e464ac891e738512962816199aa0121e19fdb3ea
Bobby Hewitt <bobby.hewitt@w12studios.com>
2018-02-21 17:04:45 +0000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+console.log('hello')
+
+// window.onscroll = function() {
+// 	console.log(window.onscroll)
+//     console.log('scrolling');
+//     var scrollPos = window.pageYOffset;
+//     var imagePos = scrollPos/2 + 'px'
+//     document.getElementById('scroller').style.left = imagePos
+// };
+
+
+
+// window.onmousemove = function(e) {
+// 	console.log(window.event.clientX)
+// 	document.getElementById('title').style.left = window.event.clientX + 'px'
+// 	document.getElementById('title').style.top = window.event.clientY + 'px'
+// }
